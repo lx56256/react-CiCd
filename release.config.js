cb1bb9e56490dcee34a7b370f1c6a04750221c87
李旭 <1766382151@qq.com>
2023-05-03 17:48:50 +0800

    feat: 构建

--- release.config.js
+++ release.config.js
@@ -1,11 +1,9 @@
 module.exports = {
-  release: {
-    branches: ['main', 'next'],
-    repositoryUrl: 'https://github.com/lx56256/react-CiCd',
-    plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      '@semantic-release/github',
-    ],
-  },
+  branches: ['main', 'next'],
+  repositoryUrl: 'https://github.com/lx56256/react-CiCd',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+  ],
 };
