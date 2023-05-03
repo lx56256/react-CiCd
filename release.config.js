b50908c48b5ebe55885a6abe8e555084abd474fd
李旭 <1766382151@qq.com>
2023-05-03 15:46:08 +0800

    feat: 增加发布版本

---
+++ release.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  release: {
+    branches: 'master',
+    repositoryUrl: 'https://github.com/lx56256/react-CiCd',
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      '@semantic-release/github'
+    ]
+  }
+}
